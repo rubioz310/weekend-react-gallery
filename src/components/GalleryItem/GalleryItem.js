66c26433174ebab3fc34803c01f6a48932d15497
Jose Rubio <ru310z@gmail.com>
2021-07-09 23:24:44 -0500

    Clicking photo will now show description and viceversa

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -1,9 +1,16 @@
+import {useState} from 'react';
+
 function GalleryItem(props) {
+    let [showDescription, setShowDescription] = useState(false);
     const photo = props.photo;
+
+    const toggleDescription = () => {
+        setShowDescription(showDescription ? false:true);
+    }
     return(
         <div key={photo.id} className="photoFrame">
-            <img src={photo.path}/>
-            <p>{photo.description}</p>
+            {!showDescription && <img src={photo.path} onClick={toggleDescription}/>}
+            {showDescription && <p onClick={toggleDescription}>{photo.description}</p>}
         </div>
     )
 }
