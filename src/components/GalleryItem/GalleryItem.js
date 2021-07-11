d10dde8f6485004d55e0f5140ec7aefd74f94c5b
Jose Rubio <ru310z@gmail.com>
2021-07-11 13:39:16 -0500

    Moved like section to its own component

--- src/components/GalleryItem/GalleryItem.js
+++ src/components/GalleryItem/GalleryItem.js
@@ -3,12 +3,8 @@ import {useState} from 'react';
 //Styles and components imports
 import './GalleryItem.css';
 import DeleteButton from '../DeleteButton/DeleteButton'
+import LikeSection from '../LikeSection/LikeSection';
 
-//Material-ui buttons and icons
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import ThumbUp from '@material-ui/icons/ThumbUp';
-import ThumbDown from '@material-ui/icons/ThumbDown';
 
 
 function GalleryItem(props) {
@@ -27,13 +23,6 @@ function GalleryItem(props) {
         setShowDelete(showDelete ? false:true);
     }
     
-    //Updates likes count with API in App.jsx
-    const handleLike = () =>{
-        likePhoto(photo.id, 1)
-    }
-    const handleDislike = () =>{
-        likePhoto(photo.id, -1)
-    }
     return(
         // onMouseOver={toggleDelete} onMouseLeave={toggleDelete}
         <div className="photoFrame" onMouseEnter={toggleDelete} onMouseLeave={toggleDelete}>
@@ -42,11 +31,7 @@ function GalleryItem(props) {
                 {!showDescription && <img src={photo.path}/>}
                 {showDescription && <div className="description"><p >{photo.description}</p></div>}
             </div>
-            <div className="likesSection">
-                <IconButton color="primary" onClick={handleLike}><ThumbUp /></IconButton>
-                <p>{photo.likes && `${photo.likes} Likes` || "No likes :("} </p>
-                <IconButton color="primary" onClick={handleDislike}><ThumbDown /></IconButton>
-            </div>
+            <LikeSection likes={photo.likes} likePhoto={likePhoto} id={photo.id}/>
         </div>
     )
 }
